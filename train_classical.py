b095d30b4d31b9f0400966ad4f3f0a69f8a51b3b
Garðar Ingvarsson <gi241@cam.ac.uk>
2022-04-24 16:42:23 +0100

    Adapted train_classical to classical_hyperopt

--- train_classical.py
+++ train_classical.py
@@ -2,12 +2,15 @@ import argparse
 import cProfile
 import json
 import os
-import random
+from time import time
 import numpy as np
-import torch
-import re
+import sklearn
 import cv2
-from torch.utils.data import DataLoader
+from sklearn.decomposition import PCA
+from sklearn.kernel_approximation import Nystroem
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 import pickle
 
 from detection import models, utils, datasets, evaluation
@@ -43,7 +46,9 @@ def get_args_parser(add_help=True):
     parser.add_argument("--cpb", default=(3, 3), nargs="+", type=int, help="cells per block in HOG")
     parser.add_argument("--block-norm", default="L2-Hys", type=str, help="block norm in HOG")
     parser.add_argument("--gamma-corr", action="store_true", help="use gamma correction in HOG")
-    parser.add_argument("--hog-dim", default=(64, 64), nargs="+", type=int, help="input dimensions for HOG extractor")
+    parser.add_argument("--no-gamma-corr", action="store_false", help="don't use gamma correction in HOG")
+    parser.set_defaults(gamma_corr=True)
+    parser.add_argument("--hog-dim", default=(128, 128), nargs="+", type=int, help="input dimensions for HOG extractor")
 
     # Sliding window parameters
     parser.add_argument("--downscale-factor", default=1.25, type=float,
@@ -59,28 +64,24 @@ def get_args_parser(add_help=True):
     parser.add_argument("--output-dir", default=".", type=str, help="path to save outputs")
     parser.add_argument("--plot-pc", action="store_true", help="plot precision recall curves")
 
-    # SVM parameters
-    parser.add_argument("--C", default=1.0, type=float, help="C parameter for SVM")
-    parser.add_argument("--loss", default="hinge", type=str, help="loss type for SVM")
-    parser.add_argument("--max-iter", default=1000, type=int, help="maximum iterations in training SVM")
-
     # Hard negative mining parameters
     parser.add_argument("--neg-per-img", default=50, type=int, help="how many hard negatives to mine from each image")
 
     # Multicore parameters
-    parser.add_argument("--cpus", default=1, type=int, help="number of cores to use for parallel operations")
+    parser.add_argument("--n-cpus", default=1, type=int, help="number of cores to use for parallel operations")
     return parser
 
 
 def main(args):
-    # TODO: Check arguments
-
     utils.initialise_logging(args)
     logging.info("Started")
 
     if args.seed is not None:
         utils.make_deterministic(args.seed)
         logging.info(f"Seed set to {args.seed}")
+
+    cv2.setUseOptimized(True)
+    cv2.setNumThreads(1)
 
     # Parse class definition file
     logging.info("Loading class definitions...")
@@ -92,73 +93,59 @@ def main(args):
     # Load datasets
     logging.info("Loading dataset...")
     train_dataset, val_dataset = datasets.load_train_val(name=args.dataset, train_path=args.train_path, classes=classes,
-                                                         val_split=args.val_split)
+                                                         val_split=args.val_split, train_transforms=None, val_transforms=None)
 
     # Create feature extractor
     logging.info("Creating feature extractor...")
     feature_extractor = models.HOG(orientations=args.hog_bins, pixels_per_cell=args.ppc, cells_per_block=args.cpb,
                                    block_norm=args.block_norm, gamma_corr=args.gamma_corr, resize_to=args.hog_dim)
 
-    # Extract features
-    logging.info("Extracting features...")
-    descriptors, labels = feature_extractor.extract_all(train_dataset, cpus=args.cpus)
+    cache_file = os.path.join(args.output_dir, 'saved_descriptors.cached.pickle')
+    if os.path.isfile(cache_file):
+        with open(cache_file, 'rb') as f:
+            descriptors, labels = pickle.load(f)
+        logging.info("Loading existing cached training data")
+    else:
+        # Extract features
+        logging.info("Extracting features...")
+        descriptors, labels = feature_extractor.extract_all(train_dataset, cpus=args.n_cpus,
+                                                            horizontal_flip=True,
+                                                            rotations=[30, -30])
+        clf = sklearn.dummy.DummyClassifier(strategy='constant', constant=1)
+        clf.fit(descriptors[:2], [0, 1])
 
-    # Add one background GT for SVM
-    logging.info(f"N={len(descriptors)},D={len(descriptors[0])}")
-    logging.info(
-        f"Min={min([descriptor.min() for descriptor in descriptors])}, Max={max([descriptor.max() for descriptor in descriptors])}")
-    descriptors.append(descriptors[0])
-    labels.append(0)
+        # Apply negative mining
+        logging.info("Performing hard negative mining")
+        negative_samples = trainingtools.mine_hard_negatives(clf, feature_extractor, train_dataset,
+                                                             iou_thresh=args.iou_thresh, max_per_img=args.neg_per_img,
+                                                             cpus=args.n_cpus)
+        descriptors = np.concatenate((descriptors, negative_samples))
+        labels = np.concatenate((labels, np.zeros(negative_samples.shape[0], dtype=np.int64)))
+        logging.info(
+            f"Added {len(negative_samples)} negatives to the {len(descriptors) - len(negative_samples)} positives")
+        with open(cache_file, 'wb') as f:
+            pickle.dump((descriptors, labels), f)
 
-    # Train SVM
-    logging.info("Training classifier on feature descriptors")
-    clf = trainingtools.train_classifier(classifier_name, descriptors, labels, num_classes,
-                                         pca_components=args.pca_components,
-                                         feature_map_gamma=args.feature_map_gamma,
-                                         feature_map_components=args.feature_map_components,
-                                         sgd_alpha=args.sgd_alpha, class_weight=args.class_weight,
-                                         fit_intercept=args.fit_intercept, max_iter=args.max_iter)
+    logging.info(f"Training on {len(descriptors)} samples with {len(descriptors[0])} dimensions")
 
-    cv2.setUseOptimized(True)
-    cv2.setNumThreads(1)
+    pca_components = 300
+    rbf_components = 2000
+    alpha = 0.01
+    gamma = 0.01
+    clf = Pipeline(steps=[('scaler', StandardScaler()),
+                          ('pca', PCA(n_components=pca_components)),
+                          ('feature_map', Nystroem(n_components=rbf_components, gamma=gamma)),
+                          ('model', SGDClassifier(alpha=alpha, max_iter=100000, early_stopping=True))])
+    start = time()
+    clf.fit(descriptors, labels)
+    fit_time = time() - start
+    logging.info(f"Fit took {fit_time:.1f} seconds")
 
     # Evaluate
     logging.info("Evaluating classifier on test dataset")
     trainingtools.evaluate_classifier(clf, feature_extractor=feature_extractor, dataset=val_dataset,
-                                      iou_thresh=args.iou_thresh, log_every=args.log_every, output_dir=args.output_dir,
-                                      plot_pc=args.plot_pc, cpus=args.cpus)
-
-    # Apply training procedure
-    for epoch in range(args.epochs):
-        # Apply hard negative mining
-        logging.info("Performing hard negative mining")
-        negative_samples = trainingtools.mine_hard_negatives(clf, feature_extractor, train_dataset,
-                                                             iou_thresh=args.iou_thresh, max_per_img=args.neg_per_img,
-                                                             cpus=args.cpus)
-
-        # Add hard negatives to training samples
-        descriptors.extend(negative_samples)
-        labels.extend([0] * len(negative_samples))
-        logging.info(
-            f"Added {len(negative_samples)} negative samples to the previous {len(descriptors) - len(negative_samples)} total")
-
-        # Save descriptors
-        with open(os.path.join(args.output_dir, f"saved_descriptors_SGDNystroem_epoch_{epoch}.pickle"), "wb") as f:
-            pickle.dump({'descriptors': descriptors, 'labels': labels}, f)
-
-        # Train SVM
-        logging.info("Training classifier on feature descriptors")
-        clf = trainingtools.train_svm(descriptors, labels, num_classes, pca_components=args.pca_components,
-                                      feature_map_gamma=args.feature_map_gamma,
-                                      feature_map_components=args.feature_map_components,
-                                      sgd_alpha=args.sgd_alpha, class_weight=args.class_weight,
-                                      fit_intercept=args.fit_intercept, max_iter=args.max_iter)
-
-        # Evaluate
-        logging.info("Evaluating classifier on test dataset")
-        trainingtools.evaluate_classifier(clf, feature_extractor=feature_extractor, dataset=val_dataset,
-                                          iou_thresh=args.iou_thresh, log_every=args.log_every,
-                                          output_dir=args.output_dir, plot_pc=args.plot_pc, cpus=args.cpus)
+                                      iou_thresh=args.iou_thresh, output_dir=args.output_dir,
+                                      plot_pc=args.plot_pc, cpus=args.n_cpus)
 
 
 if __name__ == '__main__':
